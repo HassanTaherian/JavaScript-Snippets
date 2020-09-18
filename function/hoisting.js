ca72b7d42b11815cbfb626fa9c1fd5596b30e9bf
HassanTSF <hassantsf.dev@gmail.com>
2020-09-18 16:39:16 +0430

    class hoisting

--- function/hoisting.js
+++ function/hoisting.js
@@ -15,6 +15,15 @@ function f() {
 It means that if we use variable/function before declaration, they wouldn't throw error
 */
 
+/*
+Aproatch 2:
+Hoisting: Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their 'scope' before code execution.
+
+In JavaScript, an undeclared variable is assigned the value undefined at execution and is also of type undefined.
+In JavaScript, a ReferenceError is thrown when trying to access a previously undeclared variable.
+Variable assignment takes precedence over function declaration Function declarations take precedence over variable declarations
+*/
+
 // Hoisting in var
 console.log(name) // undefiened
 var name = 'Hassan'
@@ -52,6 +61,40 @@ const /* let */ f = function f() {
 }
 f()
 
+// Warning: Of note however, is the fact that the hoisting mechanism only moves the declaration. The assignments are left in place.
+
+/* Hoisting classes */
+// 1) Class declarations (statements)
+/* Much like their function counterparts, JavaScript class declarations are hoisted. However, they remain uninitialised until evaluation. This effectively means that you have to declare a class before you can use it. */
+
+var Frodo = new Hobbit();
+Frodo.height = 100;
+Frodo.weight = 300;
+console.log(Frodo); // Output: ReferenceError: Hobbit is not defined
+
+class Hobbit {
+  constructor(height, weight) {
+    this.height = height;
+    this.weight = weight;
+  }
+}
+
+// Much like their function counterparts, class expressions are not hoisted.
+var Square = new Polygon();
+Square.height = 10;
+Square.width = 10;
+console.log(Square); // Output: TypeError: Polygon is not a constructor
+
+var Polygon = class {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+
+
+// Note: const, let. class declaration are hoisted, but unlike var, they will be evaluated to uninitialized
 /* Articles: 
 1) https://bit.ly/3c8xabk
+2) https://bit.ly/3hQoOGR
 */
