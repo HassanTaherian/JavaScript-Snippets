921a047ad87aefccd7d365fef9e8aef9f97301da
HassanTSF <hassantsf.dev@gmail.com>
2020-09-23 14:33:13 +0330

    swap wrong description

--- function/callback.js
+++ function/callback.js
@@ -1,6 +1,14 @@
 /* Callbacks */
 /* 
-Anytime we pass a function to another function and the function is excucted in that function we would call it callbacks.
+Anytime we pass a function to another function and the function is excucted in that function we would call it callbacks or with other words callbacks are functions which they are gonna be called by another function
+
+Reason: Functions in JS are first-class. (objects)
+First-class means that something is dynamic and it can be paseed to another functions, or returned value.
+ */
+
+ /* Why we need callbacks?
+ Because the nature of JS which is an event-oriented language, Instead of waiting for response, it will pass throgh them and listen for those call backs (Asynchonous)
+ Callbacks are very useful for AJAX Requests and or fetch(Promises).
  */
 
 // Use anonymous function for callback
@@ -15,3 +23,9 @@ function double(x) {
 }
 
 const doubleArr = [1, 2, 3, 4, 5].map(double /* <- callback */);
+
+/* 
+References:
+1) https://bit.ly/2RAybQ4 (Vigool-Persian)
+2) https://bit.ly/2REPcIY (javascript.info)
+*/
--- scopes/block-scope.js
+++ scopes/block-scope.js
@@ -2,15 +2,18 @@
 /* 2) Block Scope -> Variables that are declared
 in the block body(if/else, for, ...) */
 
-// var -> Not Block Scope
-if (true) {
-  const animal = 'eel';
-  console.log(animal); //'eel'
-}
-console.log(animal); //NOT DEFINED!
-
-
 // let & const -> Block Scope
+let animal = 'deer';
+if (true) {
+  let animal = 'eel';
+  const name = 'Hassan';
+  console.log(animal, name); //'eel', 'Hassan
+}
+console.log(animal); //deer
+console.log(name); //NOT DEFINED!
+
+// var -> Not Block Scope
+var animal = 'deer';
 if (true) {
   var animal = 'eel';
   console.log(animal); //'eel'
