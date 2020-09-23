4b2ecdd99af7f1f4de930c33e332d47de7757b32
HassanTSF <hassantsf.dev@gmail.com>
2020-09-23 14:35:22 +0330

    tricks of js

---
+++ tricks.js
@@ -0,0 +1,38 @@
+
+//1) Use !! operator to convert to boolean
+
+// It converts falsy values(0, '', null, etc) to false
+function validateCash(cash) {
+  if (!!cash) {
+    return 'Invalid Input';
+  } else if (cash < 0) {
+    return 'Negative Input';
+  } else {
+    return 'Valid Input';
+  }
+}
+
+// Use the + operator to convert to numbers
+let num = '74';
+num = +num;
+console.log(num, typeof num); // 2, Number
+num = +'a';
+console.log(num, typeof num); // NaN, Number
+
+// Convert date to total seconds from 1/1/1970
+console.log(+new Date());
+
+// Combine if and functions
+let decimal = 10.2;
+if(decimal % 1 !== 0) {
+  console.log(Math.floor(decimal));
+}
+
+let decimal = 10.2;
+decimal % 1 !== 0 && console.log(Math.floor(decimal));
+
+console.log(decimal);
+
+
+
+// Reference https://bit.ly/2H4w593
