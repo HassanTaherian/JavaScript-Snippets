0a38ce9365f7c95ceb58392126f1f338d2b5ec92
HassanTSF <hassantsf.dev@gmail.com>
2020-10-05 00:28:17 +0330

    custom error

--- all-relative/error-handling.js
+++ all-relative/error-handling.js
@@ -17,7 +17,7 @@ try {
   // URIError
   decodeURIComponent('%')
 
-  // Custom error
+  // Custom error messages
   if (user.name.length < 3) {
     throw new Error('Short Length!' /* error message */);
   }
@@ -38,4 +38,56 @@ try {
 
 } finally {
   console.log("This will run regardless of return or uncaught error ,...")
+}
+
+// Note: We can't use multiple catch block in Js. We have to user if/else to check type of error
+
+/* Custom Errors */
+class StringError extends Error {
+  constructor(messaage) {
+    super(messaage)
+    this.name = this.constructor.name;
+  }
+}
+class ShortLengthError extends StringError {
+  constructor() {
+    super('Your string is too short!');
+    // Short way
+    this.name = this.constructor.name;
+  }
+}
+
+class BadGrammerError extends StringError {
+  constructor() {
+    super('Your string doesn\'t start with uppercase letter!');
+    // Tidious way
+    this.name = 'BadGrammer';
+  }
+}
+
+try {
+  const name = 'HT';
+  if (name.length < 5) {
+    throw new ShortLengthError();
+  } if (name.charAt(0) < 90) {
+    throw new BadGrammerError();
+  }
+} catch (e) {
+  if (e instanceof ShortLengthError) {
+    console.log(e.message + 1)
+  } else if (e instanceof BadGrammerError) {
+    console.log(e.message + 2)
+  } else if (e instanceof StringError) {
+    console.log(e.message + 3)
+  } else {
+    throw e;
+  }
+
+  // or
+  if (e.name === 'BadGrammer') {
+    // Do
+  }
+
+  // First way is better because in the future if we inherit more class form this class, all of them will work in this condition
+  console.log(e.name);
 }
